789921874ab6ffdefc77214237e53e7982174150
Andre Sakiyama <andre.tsuyoshi@gmail.com>
2016-03-09 12:58:38 -0300

    makefile terminado

---
+++ exercicio1/calc_primo.h
@@ -0,0 +1,3 @@
+
+
+int primo(int n);
---
+++ exercicio1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc_primo.h"
+
+main(int argc, char *argv[ ])
+{
+
+  int n = atoi (argv[1]);
+  int i;
+  int nPrimos = 0;
+
+  for (i = 2; i < n; i++) {
+
+    if (primo (i)) {
+      nPrimos++;
+    }
+
+  }
+
+  printf("%d\n", nPrimos);
+
+  // int n = 104395301;
+
+  // if (primo(n))
+  //   printf("%d é primo.\n", n);
+  // else
+  //   printf("%d não é primo.\n", n);
+}	
